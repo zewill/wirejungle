91f3ce6f074a6360ba82a3dd7cc36de17d6a155b
ami.chayun <ami.chayun@111aac38-6253-11de-8ac7-2bb2ed267d8e>
2009-06-26 14:09:41 +0000

    Added license

--- libraries/ShiftRegister/ShiftRegister.h
+++ libraries/ShiftRegister/ShiftRegister.h
@@ -1,3 +1,13 @@
+/*
+	Copyright 2009 Ami Chayun
+	
+	Library to drive 74HC165 shift register
+	
+	This file is free software; you can redistribute it and/or modify
+	it under the terms of either the GNU General Public License version 2
+	or the GNU Lesser General Public License version 2.1, both as
+	published by the Free Software Foundation.
+*/
 #ifndef ShiftRegister_h
 #define ShiftRegister_h
 #include <inttypes.h>
